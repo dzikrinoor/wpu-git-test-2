03949813ee7be47aa92488cee5f8a598a450c629
dzikrinoor <muhdzikrinoor@gmail.com>
2023-11-18 21:19:43 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et rerum aperiam maxime minima consequuntur suscipit adipisci, fuga voluptatem blanditiis, autem cumque. Similique minima sed atque sit, repellat deleniti minus molestiae.</p>
+</body>
+</html>
